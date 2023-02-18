65be0c499a571b4b915d86b0fe184363762c64fa
Kshitij Verma <kshitijv250@gmail.com>
2023-02-18 22:27:11 +0530

    added highlights section

---
+++ src/components/Pic.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pic = () => {
+    return (
+        <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold p-4 my-4">
+            pic 1
+        </h1>
+        </div>
+    );
+    }
+
+export default Pic;
--- src/pages/Past.jsx
+++ src/pages/Past.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import Features from "../components/Features";
 
 const Past = () => {
   return (
-    <div className="text-white text-4xl z-50 relative bg-indigo-900 w-screen">
+    <div className="text-white z-50 relative bg-indigo-900 w-screen">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold p-4 my-4">
           <span className="grad-text">AceHack 1.0</span>  Highlights!
         </h1>
-        <div className="rounded-[50%] bg-red-500 p-4 m-3">ab</div>
+        <div className="flex flex-row justify-evenly w-full p-10">
+          <Features icon="1..." text="topic"/>
+          <Features icon="2..." text="topic"/>
+          <Features icon="3..." text="topic"/>
+          <Features icon="4..." text="topic"/>
+        </div>
+        <div className="grid grid-cols-6 grid-rows-2">
+          <div className="col-start-1 row-start-1">
+            <h1 className="text-4xl font-bold p-4 my-4">
+              pic 1
+            </h1>
+          </div>
+          <div className="col-start-1 row-start-2">
+            <h1 className="text-4xl font-bold p-4 my-4">
+              pic 1
+            </h1>
+          </div>
+          <div className="col-start-6">
+            <h1 className="text-4xl font-bold p-4 my-4">
+              pic 1
+            </h1>
+          </div>
+          <div className="col-start-6">
+            <h1 className="text-4xl font-bold p-4 my-4">
+              pic 1
+            </h1>
+          </div>
+        </div>
       </div>
     </div>
   );
