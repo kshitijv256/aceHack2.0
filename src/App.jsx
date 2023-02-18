4e0a454a5c0a6bea936d93024eaf8dcd0739818a
Devdeep Ghosh <devarmforce01@gmail.com>
2023-02-18 16:40:53 +0530

    homepage button navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <Home />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from "react";
+import FancyButton from "./FancyButton";
 
 export const Navbar = () => {
   return (
-    <p>Navbar</p>
+    <div className="w-full p-5 bg-purple-800 flex justify-between content-center">
+      <div>
+        <img src="https://via.placeholder.com/200x50.png?text=AceHack2.0" alt="AceHack2.0" />
+      </div>
+      <div className="w-full flex gap-10 justify-end">
+        <FancyButton link={"./computer"} data={"Button 1"}/>
+        <FancyButton link={"./computer"} data={"Button 2"}/>
+        <FancyButton link={"./computer"} data={"Button 3"}/>
+      </div>
+    </div>
   )
 }
 
