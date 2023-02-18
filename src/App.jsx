80272940e3b64123cde937d9938dd17642f04dc7
Devdeep Ghosh <devarmforce01@gmail.com>
2023-02-18 17:07:40 +0530

    about page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Home />
+      <About />
     </>
   )
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex flex-col text-white text-center p-5">
+      <h1 className="text-4xl font-bold">AceHack 2.0 Hackathon is here</h1>
+      <p>
+      This March, ACM-UEM Chapter proudly announces the comeback of our in-person hackathon with AceHack 2.0!
+      <br />
+      We aspire to empower our student community and encourage students to realize their potential in safe spaces.
+      <br />
+      Our hackathon`s motto, “Dream It, Hack It”, epitomizes these aspirations and encourages hackers to free themselves from their inhibitions and make their dreams come to life. 
+      <br />
+      We believe the most significant prize any student can win is knowing that they have what it takes to create beautiful work from the most basic blocks: ones and zeros.
+      </p>
+    </div>
+  )
+}
+
+export default About;
