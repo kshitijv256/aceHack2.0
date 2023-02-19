f0fdcbd17ba0e38ce6e4c8efff4b807970a11287
Devdeep Ghosh <devarmforce01@gmail.com>
2023-02-19 09:52:17 +0530

    acehack optimized image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import AceHackLogo from "/assets/images/AceHack-logo.png"
+import AceHackLogo from "/assets/images/AceHack-logo.png";
+import backgroundImage from "/assets/images/background-image.jpg";
 
 const Home = () => {
   return (
     <>
       <div className="overlay"></div>
-      <video className="bgVideo" disablePictureInPicture playsInline autoPlay muted loop>
-        <source src="/assets/videos/Circuit.mp4" type="video/mp4" />
-      </video>
+        <img className="bgVideo" src={backgroundImage} alt="aceHack background image" />
       <div className="my-4 flex flex-col justify-center items-center text-white">
-        <img alt="AceHack Logo" className="h-40 lg:h-56 w-auto" src={AceHackLogo} />
+        <img alt="AceHack Logo" className="w-auto pt-10" src={AceHackLogo} />
         <h1 className="text-2xl mt-2">Coming Soon...</h1>
         <a className="border-2 px-2 py-[1.5] m-2 border-white transition-shadow hover:bg-white hover:text-black" href={"#"}>
           <h2>Go to Events</h2>
