d78104c68647f3b85eba99ea8f96fcc944429109
Devdeep Ghosh <devarmforce01@gmail.com>
2023-02-19 10:41:13 +0530

    homepage alignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,15 @@ import backgroundImage from "/assets/images/background-image.jpg";
 
 const Home = () => {
   return (
-    <div className="sticky z-0 top-24">
+    <div className="sticky z-0 mt-36 text-white">
       <div className="overlay"></div>
         <img className="bgVideo" src={backgroundImage} alt="aceHack background image" />
-      <div className="my-4 flex flex-col justify-center items-center text-white">
-        <img alt="AceHack Logo" className="w-auto pt-10" src={AceHackLogo} />
-        <h1 className="text-2xl mt-2">Coming Soon...</h1>
-        <a className="border-2 px-2 py-[1.5] m-2 border-white transition-shadow hover:bg-white hover:text-black" href={"#"}>
-          <h2>Go to Events</h2>
+      <div className="my-10 flex flex-col justify-center items-center">
+        <img alt="AceHack Logo" className="w-auto py-10" src={AceHackLogo} />
+        <a className="border-2 px-3 py-2 p-10 text-xl border-white transition-shadow hover:bg-white hover:text-black" href={"#"}>
+          <h2>Register Now</h2>
         </a>
-
-        <h2 className="break-words text-center">March, 17 and 18 @ University of Engineering and Management, Jaipur</h2>
-
-        <h2 className="break-words text-center mb-4 mt-2">
+        <h2 className="break-words text-center my-10 text-xl">
           March, 17 and 18 @ University of Engineering and Management, Jaipur
         </h2>
       </div>
