d14de3b44a04bdf233e2dd789306d0850d16281b
Devdeep Ghosh <devarmforce01@gmail.com>
2023-02-19 12:59:05 +0530

    Pictures component added

---
+++ src/components/Pictures.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Pictures = ({imgSrc}) => {
+  return (
+    <div class="shrink-0">
+      <img src={imgSrc}
+        className="rounded-lg shadow-xl hover:shadow-cyan-700 h-56 w-auto" />
+    </div>
+  )
+}
+
+export default Pictures;
--- src/pages/Past.jsx
+++ src/pages/Past.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Features from "../components/Features";
+import Pictures from "../components/Pictures";
 
 const Past = () => {
   return (
@@ -15,31 +16,11 @@ const Past = () => {
           <Features icon="4..." text="topic" />
         </div>
         <h1 className="grad-text text-4xl font-bold py-6">Pictures</h1>
-        <div className="relative w-full flex gap-6 overflow-x-auto pb-14 scrollbar-hide hover:scroll-smooth">
-          <div class=" shrink-0 mx-2">
-            <img src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
-              className="rounded-lg shadow-xl hover:shadow-cyan-700" />
-          </div>
-          <div class=" shrink-0 mx-2">
-            <img src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
-              className="rounded-lg shadow-xl hover:shadow-cyan-700" />
-          </div>
-          <div class=" shrink-0 mx-2">
-            <img src="https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
-              className="rounded-lg shadow-xl hover:shadow-cyan-700" />
-          </div>
-          <div class=" shrink-0 mx-2">
-            <img src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
-              className="rounded-lg shadow-xl hover:shadow-cyan-700" />
-          </div>
-          <div class=" shrink-0 mx-2">
-            <img src="https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
-              className="rounded-lg shadow-xl hover:shadow-cyan-700" />
-          </div>
-          <div class=" shrink-0 mx-2">
-            <img src="https://images.unsplash.com/photo-1559333086-b0a56225a93c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
-              className="rounded-lg shadow-xl hover:shadow-cyan-700" />
-          </div>
+        <div className="relative w-full flex gap-6 justify-center overflow-x-auto pb-14 scrollbar-hide hover:scroll-smooth">
+          <Pictures imgSrc={"https://plus.unsplash.com/premium_photo-1663040077027-d74f3beaa46a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}/>
+          <Pictures imgSrc={"https://plus.unsplash.com/premium_photo-1663040077027-d74f3beaa46a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}/>
+          <Pictures imgSrc={"https://plus.unsplash.com/premium_photo-1663040077027-d74f3beaa46a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}/>
+          <Pictures imgSrc={"https://plus.unsplash.com/premium_photo-1663040077027-d74f3beaa46a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}/>
         </div>
       </div>
     </div>
