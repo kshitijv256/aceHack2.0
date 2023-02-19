88c3f2908bad449f2f4e1b777a74d6bea192d10c
Devdeep Ghosh <devarmforce01@gmail.com>
2023-02-19 11:42:55 +0530

    about page edit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const About = () => {
   return (
     <div className="bg-gradient-to-br from-indigo-900/50 via-blue-900/50 to-cyan-900/50 backdrop-blur-lg scroll-smooth w-screen">
-      <div className="flex flex-col text-white text-center p-5 font-mono">
-        <h1 className="text-4xl font-bold p-4">
+      <div className="flex flex-col text-white text-center items-center">
+        <h1 className="text-6xl font-bold py-10">
           <span className="grad-text">AceHack 2.0</span>  Hackathon is here!
         </h1>
-        <div className="text-xl mx-6 md:mx-56 text-justify mt-6 space-y-4">
+        <div className="text-xl flex flex-col gap-5 lg:px-56 pb-10 px-5">
           <p>
             This March, ACM-UEM Chapter proudly announces the comeback of our
             in-person hackathon with AceHack 2.0!
