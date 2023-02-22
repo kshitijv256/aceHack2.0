9b7e11b9ad8c07be4fc39c18e5135ab784fcfbec
Devdeep Ghosh <devarmforce01@gmail.com>
2023-02-22 16:47:45 +0530

    mobile navigation bar configured

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,20 +8,37 @@ import { MdOutlineClose } from "react-icons/md";
 export const Navbar = () => {
 
   const [closed, setClosed] = useState(true);
-  
+  const [scale, setScale] = useState("scale-0");
+  const [height, setHeight] = useState("h-0");
+
+  const clickHandler = () => {
+    if (height == "h-0") setHeight("min-h-screen");
+    else if(height == "min-h-screen") setHeight("h-0");
+    if (scale === "scale-0") setScale("scale-100");
+    else if (scale === "scale-100") setScale("scale-0");
+    setClosed(!closed)
+  }
+
   return (
-    <div className="bg-gray-900/30 w-full fixed top-0 z-30 backdrop-blur-md">
+    <div className={`bg-gray-900/30 w-full fixed top-0 z-30 backdrop-blur-md md:h-auto ${height}`}>
       <IconContext.Provider
         value={{ style: { fontSize: "2rem", position: "relative", color: "white" } }}
       >
-        <div onClick={() => setClosed(!closed)}>
+        <div onClick={clickHandler}>
           {(closed && <FaBars className="md:hidden w-10 h-10 pl-4 py-1" />) ||
-          (!closed && <MdOutlineClose className="md:hidden w-10 h-10 pl-4 py-1" />)}
+            (!closed && <MdOutlineClose className="md:hidden w-10 h-10 pl-4 py-1" />)}
         </div>
         <div>
           <div className="flex justify-between items-center">
             <img src={AceHackSm} alt="AceHack2.0" className="hidden md:block object-contain" />
-            <div className="hidden space-x-6 md:flex items-center px-6">
+            <div className="hidden space-x-6 md:flex flex-row items-center px-6">
+              <FancyButton id={"home"} data={"Register!"} />
+              <FancyButton id={"about"} data={"About"} />
+              <FancyButton id={"past"} data={"Acehack 1.0"} />
+              <FancyButton id={"themes"} data={"Themes"} />
+              <FancyButton id={"team"} data={"Team"} />
+            </div>
+            <div className={`${scale} md:hidden gap-6 flex flex-col items-center py-20 w-full transition ease-in delay-100 duration-200`}>
               <FancyButton id={"home"} data={"Register!"} />
               <FancyButton id={"about"} data={"About"} />
               <FancyButton id={"past"} data={"Acehack 1.0"} />
