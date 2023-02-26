c741c5197209cfed951f08789600c596479ffcf9
Kshitij Verma <kshitijv250@gmail.com>
2023-02-26 21:59:55 +0530

    changed hackathon date

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AceHackLogo from "/assets/images/AceHack-logo.png";
 import backgroundImage from "/assets/images/background-image.jpg";
 import {ImLocation2} from "react-icons/im";
+import FancyButton from "../components/FancyButton";
 
 const Home = () => {
   return (
@@ -10,12 +11,12 @@ const Home = () => {
         <img className="bgVideo" src={backgroundImage} alt="aceHack background image" />
       <div className="my-10 flex flex-col justify-center items-center">
         <img alt="AceHack Logo" className="py-10 max-w-[80%]" src={AceHackLogo}/>
-        <a className="border-2 px-3 py-2 p-10 text-xl border-white transition-shadow hover:bg-gradient-to-br from-cyan-300 to-cyan-500 hover:text-black" href={"#"}>
-          <h2>Register Now</h2>
+        <a className="text-black text-xl border-white  hover:text-white" href={"#"}>
+          <FancyButton data="Register Now" id="register" />
         </a>
         <h2 className="break-words text-center mt-8 mb-10 text-2xl flex items-center">
           <ImLocation2 className="w-10 fill-blue-600" />
-          <p> &nbsp; March, 17 and 18 @ University of Engineering and Management, Jaipur</p>
+          <p> &nbsp; April 1<sup>st</sup> and 2<sup>nd</sup> @ University of Engineering and Management, Jaipur</p>
         </h2>
       </div>
     </div>
