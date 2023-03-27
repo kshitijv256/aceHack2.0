6a220661e01bd044c8fadf30ec289d98dee8a150
Kshitij Verma <kshitijv250@gmail.com>
2023-03-28 01:25:59 +0530

    updated highlights

--- src/pages/Past.jsx
+++ src/pages/Past.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Features from "../components/Features";
 import Pictures from "../components/Pictures";
-import { FaUsers, FaUniversity, FaEnvelopeOpenText } from "react-icons/fa";
+import { FaUsers, FaUniversity, FaEnvelopeOpenText, FaCity } from "react-icons/fa";
 import { IoRocket } from "react-icons/io5";
 import { IconContext } from "react-icons";
 
@@ -24,10 +24,10 @@ const Past = () => {
           <span className="grad-text">AceHack 1.0</span> Highlights!
         </h1>
         <div className="flex sm:flex-row justify-evenly w-full p-10 flex-col">
-          <Features icon={<FaEnvelopeOpenText/>} count="1000" text="Applications" color="blue" />
-          <Features icon={<FaUsers/>} count="300" text="Participants" color="emerald" />
-          <Features icon={<FaUniversity/>} count="10" text="Universities" color="blue"/>
-          <Features icon={<IoRocket/>} count="80" text="Projects" color="emerald"/>
+          <Features icon={<FaEnvelopeOpenText/>} count="1700" text="Applications" color="blue" />
+          <Features icon={<FaUsers/>} count="500" text="Participants" color="emerald" />
+          <Features icon={<FaUniversity/>} count="40" text="Universities" color="blue"/>
+          <Features icon={<FaCity/>} count="20" text="Cities" color="emerald"/>
         </div>
         <h1 className="grad-text text-5xl font-bold py-6">Glimpses</h1>
         
