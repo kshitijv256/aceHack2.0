a03f4fb871f25f71a2ad0b10a10654a5d844d6f5
Kshitij Verma <kshitijv250@gmail.com>
2023-04-01 09:34:27 +0530

    added timer

--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const schedule = [
-  [Date("2023-04-01T11:30:00"), "11:30:00", "Hackathon Begins!"],
+  [],
+  [Date("2023-04-01T11:30:00"), "12:00:00", "Hackathon Begins!"],
   [Date("2023-04-01T13:00:00"), "13:00:00", "Lunch"],
   [Date("2023-04-01T15:00:00"), "15:00:00", "Workshop (Postman)"],
   [Date("2023-04-01T16:30:00"), "16:30:00", "Mentoring Session"],
@@ -12,10 +13,8 @@ const schedule = [
   [Date("2023-04-02T09:00:00"), "09:00:00", "Breakfast"],
   [Date("2023-04-02T09:00:00"), "09:00:00", "Breakfast"],
   [Date("2023-04-02T11:00:00"), "11:00:00", "Final Mentoring Session"],
-  [Date("2023-04-02T13:00:00"), "13:00:00", "Hack Ends!"],
+  [Date("2023-04-02T13:00:00"), "12:00:00", "Hack Ends!"],
   [Date("2023-04-02T15:00:00"), "15:00:00", "Prizes + Closing Ceremony"],
-
-
 ]
 
 const Live = () => {
@@ -39,20 +38,20 @@ const Live = () => {
   }, [time]);
   return (
     <div
-      id="about"
-      className="py-12 bg-gray-900/30 backdrop-blur-lg relative scroll-smooth w-full"
+      id="live"
+      className="py-12 bg-gray-900/30 relative scroll-smooth w-full"
     >
       <div className="flex flex-col md:flex-row text-lg text-white gap-10 mx-10 justify-between">
         <div className="flex flex-col items-start">
+        <h1 className="text-5xl font-semibold pb-12 grad-text">
+          {time.toLocaleTimeString()}
+        </h1>
           <p>Happening Now</p>
         <h1 className="text-5xl font-bold pt-2 pb-12 grad-text">
           {schedule[currentEvent][2]}
         </h1>
-        <h1 className="text-5xl font-semibold pb-12">
-          {time.toLocaleTimeString()}
-        </h1>
         <p>Next</p>
-        <div className="flex flex-row">
+        <div className="flex flex-row items-center">
         <div className="text-5xl font-bold pt-2 pb-2 pr-4 grad-text border-0 md:border-r-2 border-gray-300">
           {schedule[currentEvent + 1][2]}
         </div>
